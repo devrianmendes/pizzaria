46379a258dc5b1b40ef8db111523b3772ae01271
devrianmendes <dev.rianmendes@outlook.com>
2025-01-29 23:15:26 -0300

    ready before db change

--- frontend/src/services/app.ts
+++ frontend/src/services/app.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     // baseURL: "http://localhost:3333",
-    baseURL: "http://67.205.172.80:3333"
+    baseURL: "https://67.205.172.80:3333"
 });
