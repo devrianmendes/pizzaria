fc44f1ed108ad34b923af523a62097c3eed2c884
devrianmendes <dev.rianmendes@outlook.com>
2025-02-28 00:33:08 -0300

    dns changed to devrianmendes

--- frontend/src/services/app.ts
+++ frontend/src/services/app.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
     // baseURL: "http://localhost:3333",
     // baseURL: "http://67.205.172.80:3333"
-    baseURL: "https://apizzaria.duckdns.org"
+    baseURL: "https://devrianmendes.duckdns.org/pizzaria"
 });
